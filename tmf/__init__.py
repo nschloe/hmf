7cf872edc9414233b2036c4ef757d40b94cedafb
Nico Schlömer <nico.schloemer@gmail.com>
2019-12-21 15:00:15 +0100

    include __about__

--- tmf/__init__.py
+++ tmf/__init__.py
@@ -1,4 +1,13 @@
 from . import _cli
+from .__about__ import __author__, __email__, __version__, __website__
 from ._main import read, write
 
-__all__ = ["read", "write", "_cli"]
+__all__ = [
+    "read",
+    "write",
+    "_cli",
+    "__author__",
+    "__email__",
+    "__version__",
+    "__website__",
+]
